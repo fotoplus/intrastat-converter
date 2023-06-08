fc2745391dfe32790a595298d594a6581fba8741
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2023-06-08 15:38:27 +0200

    Update index.php

--- index.php
+++ index.php
@@ -431,6 +431,36 @@ if ($csv) {
 				background-color: var(--color-red);
 				color: white;
 			}
+
+			footer {
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				text-align: center;
+				margin-top: 20px;
+				margin-bottom: 3rem;
+				color: black;
+				font-size: 1em;
+			}
+
+			footer svg {
+				margin-left: 10px;
+				margin-right: 5px;
+				opacity: 0.5;
+			}
+
+			footer a {
+				display: flex;
+				align-items: center;
+				color: black;
+				text-decoration: none;
+				opacity: 0.5;
+			}
+			
+			footer a:hover {
+				opacity: 1;
+			}
+
 		</style>
 	</head>
 
@@ -537,7 +567,16 @@ if ($csv) {
 				<button type="reset">Űrlap adatainak törlése</button>
 			</div>
 		</form>
-
+		<footer>
+			<svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-mark-github">
+    			<path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
+			</svg>
+			<a href="https://github.com/borbasmatyas" target="_blank">@borbasmatyas</a> 
+ 			<svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-repo color-fg-muted mr-2">
+    			<path d="M2 2.5A2.5 2.5 0 0 1 4.5 0h8.75a.75.75 0 0 1 .75.75v12.5a.75.75 0 0 1-.75.75h-2.5a.75.75 0 0 1 0-1.5h1.75v-2h-8a1 1 0 0 0-.714 1.7.75.75 0 1 1-1.072 1.05A2.495 2.495 0 0 1 2 11.5Zm10.5-1h-8a1 1 0 0 0-1 1v6.708A2.486 2.486 0 0 1 4.5 9h8ZM5 12.25a.25.25 0 0 1 .25-.25h3.5a.25.25 0 0 1 .25.25v3.25a.25.25 0 0 1-.4.2l-1.45-1.087a.249.249 0 0 0-.3 0L5.4 15.7a.25.25 0 0 1-.4-.2Z"></path>
+			</svg>
+			<a href="https://github.com/fotoplus/intrastat-converter" target="_blank">fotoplus/intrastat-converter</a>
+		</footer>
 	</body>
 
 	</html>
